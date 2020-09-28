3e4f4ce013031b2b9af6c186fd5b46c9e4932355
bru <brahada25@gmail.com>
2020-09-28 19:50:24 +0530

    changes with database

--- plaid-django-celery/plaid_django/settings.py
+++ plaid-django-celery/plaid_django/settings.py
@@ -74,36 +74,7 @@ WSGI_APPLICATION = 'plaid_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if os.getenv('GAE_APPLICATION', None):
-    # Running on production App Engine, so connect to Google Cloud SQL using
-    # the unix socket at /cloudsql/<your-cloudsql-connection string>
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': os.getenv('DATABASE_HOST'),
-            'NAME': 'plaid_db',
-            'USER': os.getenv('DATABASE_USER'),
-            'PASSWORD': os.getenv('DATABASE_PASS'),
-        }
-    }
-else:
-    # Running locally so connect to either a local MySQL instance or connect to
-    # Cloud SQL via the proxy. To start the proxy via command line:
-    #
-    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
-    #
-    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.mysql',
-    #         'HOST': '127.0.0.1',  # DataBase's IP address
-    #         'PORT': '3306',
-    #         'NAME': 'plaid_db',
-    #         'USER': os.getenv('DATABASE_USER'),
-    #         'PASSWORD': os.getenv('DATABASE_PASS'),
-    #     }
-    # }
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
